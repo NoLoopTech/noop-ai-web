4f31b4bf02d0b77a28a93e48bf20c85b485e04c2
PasinDeveloper <jobs.pasindu@gmail.com>
2025-10-26 16:09:56 +0530

    conflict resolve and update files to current

--- src/app/(portal)/[lng]/admin/dashboard/[projectCode]/bot-settings/chat-interface/partials/ChatInterfaceStyles.tsx
+++ src/app/(portal)/[lng]/admin/dashboard/[projectCode]/bot-settings/chat-interface/partials/ChatInterfaceStyles.tsx
@@ -4,7 +4,6 @@ import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle
@@ -500,25 +499,17 @@ const ChatInterfaceStyles = ({
         animate="animate"
         exit="exit"
         transition={{ duration: 0.3 }}
-        className="min-w-[630px] space-y-4"
+        className="space-y-4"
       >
         <ScrollArea scrollbarVariant="tiny" className="h-[calc(100vh-265px)]">
           <div className="flex flex-col space-y-5 pt-1 pr-3.5 pb-5">
             <Form {...form}>
-              <form
-                id="chat-interface-style"
-                className="flex flex-col gap-4 overflow-auto"
-              >
-                <Card className="w-full transition-all duration-300 ease-in-out hover:border-1 hover:border-slate-300 hover:bg-zinc-300/25 hover:shadow-lg dark:hover:border-slate-700/90 dark:hover:bg-slate-900/50">
+              <form id="chat-interface-style" className="flex flex-col gap-4">
+                <Card>
                   <CardHeader className="px-5 pt-5">
                     <CardTitle className="text-xl font-semibold">
                       Brand Styling
                     </CardTitle>
-                    <CardDescription>
-                      Customize your bot’s look to match your brand. Update the
-                      bot profile image and primary color for a consistent
-                      visual identity
-                    </CardDescription>
                   </CardHeader>
                   <CardContent className="space-y-5 px-5 pt-0 pb-5">
                     <FormField
@@ -677,7 +668,7 @@ const ChatInterfaceStyles = ({
                   </CardContent>
                 </Card>
 
-                <Card className="w-full transition-all duration-300 ease-in-out hover:border-1 hover:border-slate-300 hover:bg-zinc-300/25 hover:shadow-lg dark:hover:border-slate-700/90 dark:hover:bg-slate-900/50">
+                <Card>
                   <CardHeader className="px-5 pt-5">
                     <CardTitle className="text-xl font-semibold">
                       Chat Appearance{" "}
@@ -685,11 +676,6 @@ const ChatInterfaceStyles = ({
                         (Coming Soon)
                       </span>
                     </CardTitle>
-
-                    <CardDescription>
-                      Control how your chat interface looks. Switch between
-                      light and dark themes to match your website’s style.
-                    </CardDescription>
                   </CardHeader>
                   <CardContent className="space-y-5 px-5 pt-0 pb-6">
                     <FormField
@@ -709,7 +695,7 @@ const ChatInterfaceStyles = ({
                                 <FormControl>
                                   <FormLabel htmlFor="light">
                                     <Card className="hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary cursor-pointer rounded-xl duration-300 ease-in-out transform-fill">
-                                      <CardContent className="m-0 rounded-t-[12px] bg-zinc-50 p-0 pt-2 pl-3">
+                                      <CardContent className="m-0 rounded-t-[12px] bg-zinc-50 p-0 pt-2.5 pl-5">
                                         <LightIcon />
                                       </CardContent>
                                       <CardFooter className="flex items-center justify-between border-t py-5">
@@ -729,7 +715,7 @@ const ChatInterfaceStyles = ({
                                 <FormControl>
                                   <FormLabel htmlFor="dark">
                                     <Card className="_hover:bg-accent peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary _cursor-pointer cursor-not-allowed rounded-xl duration-300 ease-in-out transform-fill">
-                                      <CardContent className="m-0 rounded-t-[12px] bg-zinc-800 p-0 pt-2 pl-3">
+                                      <CardContent className="m-0 rounded-t-[12px] bg-zinc-800 p-0 pt-2.5 pl-5">
                                         <DarkIcon />
                                       </CardContent>
                                       <CardFooter className="flex items-center justify-between border-t py-5">
@@ -754,17 +740,17 @@ const ChatInterfaceStyles = ({
                   </CardContent>
                 </Card>
 
-                <Card className="w-full transition-all duration-300 ease-in-out hover:border-1 hover:border-slate-300 hover:bg-zinc-300/25 hover:shadow-lg dark:hover:border-slate-700/90 dark:hover:bg-slate-900/50">
+                <Card>
                   <CardHeader className="px-5 pt-5">
-                    <CardTitle className="text-xl font-semibold">
-                      Chat Icon Appearance
-                    </CardTitle>
-
-                    <CardDescription>
-                      Personalize the chat launcher. Upload your own icon,
-                      choose background colors, and set its position on your
-                      site (left or right)
-                    </CardDescription>
+                    <div className="flex items-center justify-between">
+                      <div className="flex w-full items-center justify-between">
+                        <div className="flex flex-col space-y-1">
+                          <CardTitle className="text-xl font-semibold">
+                            Chat Button Appearance
+                          </CardTitle>
+                        </div>
+                      </div>
+                    </div>
                   </CardHeader>
                   <CardContent className="space-y-5 px-5 pt-0 pb-6">
                     <FormField
@@ -863,7 +849,7 @@ const ChatInterfaceStyles = ({
                         <FormItem>
                           <div className="flex items-center justify-between space-y-0">
                             <div className="flex flex-col space-y-1">
-                              <FormLabel>Chat icon button color</FormLabel>
+                              <FormLabel>Chat button color</FormLabel>
                               <FormMessage />
                             </div>
                             <FormControl>
@@ -896,7 +882,7 @@ const ChatInterfaceStyles = ({
                         <FormItem hidden>
                           <div className="flex items-center justify-between space-y-0">
                             <div className="flex flex-col space-y-1">
-                              <FormLabel>Chat icon text color</FormLabel>
+                              <FormLabel>Chat bubble text color</FormLabel>
                               <FormMessage />
                             </div>
                             <FormControl>
@@ -919,7 +905,7 @@ const ChatInterfaceStyles = ({
                         <FormItem>
                           <div className="flex items-center justify-between space-y-0">
                             <div className="flex flex-col space-y-1">
-                              <FormLabel>Chat icon border color</FormLabel>
+                              <FormLabel>Chat button border color</FormLabel>
                               <FormMessage />
                             </div>
                             <FormControl>
@@ -949,13 +935,13 @@ const ChatInterfaceStyles = ({
                       name="chatButtonPosition"
                       render={({ field }) => (
                         <FormItem className="col-span-6">
-                          <FormLabel>Aligns chat icon</FormLabel>
+                          <FormLabel>Aligns chat button</FormLabel>
                           <FormControl>
                             <RadioGroup
                               onValueChange={field.onChange}
                               defaultValue={field.value}
                               value={field.value}
-                              className="mt-3 flex h-full w-full flex-col items-start space-y-1"
+                              className="mt-1 flex h-full w-full flex-col items-start space-y-1"
                             >
                               <FormItem>
                                 <FormControl>
@@ -995,7 +981,7 @@ const ChatInterfaceStyles = ({
                   </CardContent>
                 </Card>
 
-                <Card className="w-full transition-all duration-300 ease-in-out hover:border-1 hover:border-slate-300 hover:bg-zinc-300/25 hover:shadow-lg dark:hover:border-slate-700/90 dark:hover:bg-slate-900/50">
+                <Card>
                   <CardHeader className="px-5 pt-5">
                     <div className="flex items-center justify-between">
                       <div className="flex w-full items-center justify-between">
@@ -1024,7 +1010,7 @@ const ChatInterfaceStyles = ({
                                 <FormControl>
                                   <FormLabel htmlFor="bg-half">
                                     <Card className="hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary cursor-pointer rounded-xl duration-300 ease-in-out transform-fill">
-                                      <CardContent className="m-0 rounded-t-[12px] bg-zinc-50 px-3 pt-0 pb-0 dark:bg-slate-900">
+                                      <CardContent className="m-0 rounded-t-[12px] bg-zinc-50 px-6 pt-4 pb-0">
                                         <HalfBgIcon />
                                       </CardContent>
                                       <CardFooter className="flex items-center justify-between border-t py-5">
@@ -1044,7 +1030,7 @@ const ChatInterfaceStyles = ({
                                 <FormControl>
                                   <FormLabel htmlFor="bg-full">
                                     <Card className="hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary cursor-pointer rounded-xl duration-300 ease-in-out transform-fill">
-                                      <CardContent className="m-0 rounded-t-[12px] bg-zinc-50 px-3 pt-0 pb-0 dark:bg-slate-900">
+                                      <CardContent className="m-0 rounded-t-[12px] bg-zinc-50 px-6 pt-4 pb-0">
                                         <FullBgIcon />
                                       </CardContent>
                                       <CardFooter className="flex items-center justify-between border-t py-5">
